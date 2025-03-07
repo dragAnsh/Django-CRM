651a999a8d479a43010c19d9967450c259548c1d
Anshul Pareek <anshulpareek50@gmail.com>
2025-03-08 02:38:08 +0530

    Final Commit

--- website/views.py
+++ website/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm, AddRecordForm
+from .models import Record
 
 
 # Create your views here.
 def home(request):
+    records = Record.objects.all()
+
     # check to see if user is logging in
 
     if request.method == 'POST':
@@ -23,7 +27,7 @@ def home(request):
             return redirect('home')
         
     else:
-        return render(request, 'home.html', {})
+        return render(request, 'home.html', {'records': records})
 
 
 def logout_user(request):
@@ -33,4 +37,79 @@ def logout_user(request):
 
 
 def register_user(request):
-    return render(request, 'register.html', {})
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            # authenticate and log in the user
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            messages.success(request, "You have Successfully Registered! Welcome...")
+            return redirect('home')
+        
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
+
+
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+
+        customer_record = Record.objects.get(id = pk)
+        return render(request, 'record.html', {'customer_record': customer_record})
+    
+    else:
+        messages.success(request, "You are not authorized to view records!")
+        return redirect('home')
+    
+
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        to_delete = Record.objects.get(id = pk)
+        to_delete.delete()
+
+        messages.success(request, "Record have been Deleted Successfully!")
+
+    else:
+        messages.success(request, "You are not authorized to Delete the Record!")
+    return redirect('home')
+
+
+def add_record(request):
+    form = AddRecordForm(request.POST or None)
+
+    if request.user.is_authenticated:
+        if request.method == "POST":
+            if form.is_valid():
+                add_record = form.save()
+                messages.success(request, "Record Added Successfully!")
+                return redirect('home')
+            
+        return render(request, 'add_record.html', {'form': form})
+
+    messages.success(request, "You must be Logged In to Add a Record!")
+    return redirect('home')
+
+
+def update_record(request, pk):
+    if request.user.is_authenticated:
+        current_record = Record.objects.get(id = pk)
+
+        form = AddRecordForm(request.POST or None, instance=current_record)
+
+        if request.method == "POST":
+            if form.is_valid():
+                form.save()
+                messages.success(request, "Record Updated Successfully!")
+                return redirect('home')
+        
+        return render(request, 'update_record.html', {'form': form})
+    
+    else:
+        return redirect('home')
+
+    
